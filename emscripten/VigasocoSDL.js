59d03da8e67463500d3ebd4ada959e2786b5070a
luzbel <luzbel@users.noreply.github.com>
2025-10-19 21:55:24 +0200

    Pruebas Cross-origin isolation en gh-pages https://github.com/gzuidhof/coi-serviceworker

--- emscripten/VigasocoSDL.js
+++ emscripten/VigasocoSDL.js
@@ -83,7 +83,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmppprajt8p.js
+// include: /tmp/tmpykg9tzzg.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -217,14 +217,14 @@ Module['FS_createPath']("/roms", "abadia", true, true);
 
   })();
 
-// end include: /tmp/tmppprajt8p.js
-// include: /tmp/tmp0zicceai.js
+// end include: /tmp/tmpykg9tzzg.js
+// include: /tmp/tmpk33in8og.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp0zicceai.js
+  // end include: /tmp/tmpk33in8og.js
 // include: pre.js
 Module["preRun"].push(function () {
     addRunDependency('syncfs')
@@ -273,13 +273,13 @@ if (globalThis.window) {
   }
 }
 // end include: /home/VigasocoSDL/emsdk/upstream/emscripten/src/emrun_prejs.js
-// include: /tmp/tmpt5venl6d.js
+// include: /tmp/tmpxn3cvjal.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpt5venl6d.js
+  // end include: /tmp/tmpxn3cvjal.js
 
 
 var arguments_ = [];
